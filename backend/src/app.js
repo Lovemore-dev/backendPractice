0b8161b7bb5c9a62dbba0cf8ff9e2d804b04621c
Lovemore-dev <lovemoreodongo02@gmail.com>
2026-07-07 12:11:34 +0300

    Initialized express application

---
+++ backend/src/app.js
@@ -0,0 +1,5 @@
+import express from "express"; //Import express module
+
+const app = express(); //Create an express application
+
+export default app; //export express application for use in other modules
