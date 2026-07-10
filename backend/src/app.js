12650c5f9246b678b85497a2c0a6fa910b564194
Lovemore-dev <lovemoreodongo02@gmail.com>
2026-07-10 10:30:28 +0300

    Added a middleware to handle json, imported a user router and declared a user route

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,5 +4,17 @@ import express from "express";
 //Create an express application
 const app = express(); 
 
+// Middleware to parse incoming JSON requests
+app.use(express.json());
+
+// router import
+import userRouter from "./routes/user.route.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+
+
+// example route http://localhost:5001/api/v1
+
 //export express application for use in other modules
 export default app; 
