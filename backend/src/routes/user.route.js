85f6e1c30941aa4afbdb353b0993d8d714a31596
Lovemore-dev <lovemoreodongo02@gmail.com>
2026-07-10 10:32:22 +0300

    Imported user controller and defined post route to register users

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,8 +1,12 @@
 // Import the Router class from the express
 import { Router } from "express"; 
-
+// Import from user controller
+import { registerUser } from "../controllers/user.controller.js"; 
 // create a new router instance
 const router = Router(); 
+
+// Define a POST route for user registration
+router.route("/register").post(registerUser);
 
 // export the router instance for use in other parts of the application
 export default router; 
