fcc0a733fc528e1974927bdc12f88f790ffa6dee
Lovemore-dev <lovemoreodongo02@gmail.com>
2026-07-10 08:15:46 +0300

    Created route to allow users register

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,5 @@
+import { Router } from "express"; // Import the Router class from the express
+
+const router = Router(); // create a new router instance
+
+export default router; // export the router instance for use in other parts of the application
