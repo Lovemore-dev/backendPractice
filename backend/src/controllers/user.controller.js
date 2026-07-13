221cecc8cc307e58ade6c28e6d1b271ceb73b3ed
Lovemore-dev <lovemoreodongo02@gmail.com>
2026-07-13 15:07:29 +0300

    Added auth APIs for user login and user logout

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -38,5 +38,61 @@ const registerUser = async (req, res) => {
         res.status(500).json({ message: "Internal server error", error: error.message});
     }
 }
+
+// Function to login a User
+const loginUser = async (req, res) => {
+    try {
+        // check if user exists
+        const { email, password } = req.body;
+        const user = await User.findOne({ 
+            email: email.toLowerCase() });
+
+        // if user does not exist
+        if (!user) return res.status(404).json({
+            message: "User not found"
+        });
+
+        //compare password
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) return res.status(400).json({
+            message: "Invalid credentials"
+        })
+        res.status(200).json({
+            message: "User logged in successfully",
+            user:{
+                id: user._id,
+                email: user.email,
+                username: user.username
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error",
+            error: error.message
+        })
+    }
+}
+
+// Function to logout a User
+const logoutUser = async (req, res) => {
+    try{
+        const user = await User.findOne({
+            email: req.body.email.toLowerCase()
+        });
+
+        if (!user) return res.status(404).json({
+            message: "User not found"
+        });
+
+        res.status(200).json({
+            message: "User logged out successfully"
+        });
+    }catch (error){
+        res.status(500).json({
+            message: "Intenal server error",
+            error: error.message
+        })
+    }
+}
 // Export
-export { registerUser };
+export { registerUser, loginUser, logoutUser };
