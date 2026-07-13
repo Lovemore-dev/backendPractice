d5ecacac58930ac04300d3d6c3fe3e1295ca2375
Lovemore-dev <lovemoreodongo02@gmail.com>
2026-07-13 15:08:40 +0300

    Imported bcrypt and added a pre-save hashing middleware to hash passwords before saving to database

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,7 @@
 // Import the mongoose llibrary and the Schema class from mongoose
 import mongoose, {Schema} from "mongoose";
+// Import the bcryptjs library for password hashing
+import bcrypt from "bcrypt"
 
 // Define the user schema with the required fields and their validation rules
 const userSchema = new Schema(
@@ -31,6 +33,17 @@ const userSchema = new Schema(
         }
 
 );
+
+// Pre-save middleware to hash the password before saving the user document
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return next(); 
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+// Compare passwords
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
 
 // Export the User model based on the user
 export const User = mongoose.model("User", userSchema);
