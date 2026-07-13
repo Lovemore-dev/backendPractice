693dc0d1e71037e2f50ba26a890f483b7181d378
Lovemore-dev <lovemoreodongo02@gmail.com>
2026-07-13 15:09:07 +0300

    Added auth routes fro login and logout

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,12 +1,18 @@
 // Import the Router class from the express
 import { Router } from "express"; 
 // Import from user controller
-import { registerUser } from "../controllers/user.controller.js"; 
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js"; 
 // create a new router instance
 const router = Router(); 
 
 // Define a POST route for user registration
 router.route("/register").post(registerUser);
+
+// Define a POST route for user login
+router.route("/login").post(loginUser);
+
+// Define a POST route for user logout
+router.route("/logout").post(logoutUser);
 
 // export the router instance for use in other parts of the application
 export default router; 
